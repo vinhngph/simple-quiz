29e4b058b3fa139b7ddb1f7659096a4131480305
vinhngph <136446610+vinhngph@users.noreply.github.com>
2025-04-07 14:30:12 +0700

    feat: press space to next question

--- shortcuts.js
+++ shortcuts.js
@@ -4,6 +4,13 @@ document.addEventListener("keydown", (e) => {
 
         if (!isNaN(e.key) && e.key.trim() !== '') {
             document.querySelector(`#q${Number(e.key) - 1}`).click();
+        } else if (e.code === "Space") {
+            e.preventDefault();
+
+            const btnNext = document.querySelector(".btn-next");
+            if (!btnNext.disabled && !document.querySelector(".quiz-container").classList.contains("hidden")) {
+                btnNext.click();
+            }
         }
     }
 })
