f8c1ed8e40118db76cee8b8e10141c488000c79d
vinhngph <136446610+vinhngph@users.noreply.github.com>
2025-04-07 14:33:39 +0700

    fix: back browser shorcuts

--- shortcuts.js
+++ shortcuts.js
@@ -1,8 +1,7 @@
 document.addEventListener("keydown", (e) => {
     if (e.key) {
-        e.preventDefault();
-
         if (!isNaN(e.key) && e.key.trim() !== '') {
+            e.preventDefault();
             document.querySelector(`#q${Number(e.key) - 1}`).click();
         } else if (e.code === "Space") {
             e.preventDefault();
